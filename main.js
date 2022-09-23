f7edbc4c8440c5502a0526cdcb2439009c21397c
Ewa Czajka <czajka.ewa.anna@gmail.com>
2022-09-23 13:44:19 +0200

    adding new player

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const player = document.querySelector('.player')
+const playerBox = document.querySelector('.players')
+const addPlayer = document.querySelector('.player__add')
+const addPlayerBtn = addPlayer.querySelector('.player__add__btn')
 
+const addNewPlayer = () => {
+    const newPlayer = player.cloneNode(true)
+    addPlayer.before(newPlayer)
+
+    if (playerBox.childElementCount > 5) {
+        addPlayer.classList.add('hide')
+    }
+}
+
+addPlayerBtn.addEventListener('click', addNewPlayer)
