4c65820339b7ed69b8910fb8ab63346e012125d2
Ewa Czajka <czajka.ewa.anna@gmail.com>
2022-09-26 12:26:05 +0200

    changing profile logo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const player, playerBox, addPlayer, addPlayerBtn
+let player, playerBox, addPlayer, addPlayerBtn, players, playerLogo, playerName, tooltip
 
 const main = () => {
     prepereDOMElements()
@@ -9,11 +9,15 @@ const prepereDOMElements = () => {
     player = document.querySelector('.player')
     playerBox = document.querySelector('.players')
     addPlayer = document.querySelector('.player__add')
-    addPlayerBtn = addPlayer.querySelector('.player__add__btn')
+    addPlayerBtn = document.querySelector('.player__add__btn')
+    playerLogo = document.querySelector('.player__profile__logo')
+    playerName = document.querySelector('.player__profile__name')
+    tooltip = document.querySelector('.tooltip')
 }
 
-const prepereDOMEvenst = () => {
+const prepereDOMEvents = () => {
     addPlayerBtn.addEventListener('click', addNewPlayer)
+    playerBox.addEventListener('click', editPlayerLogo)
 }
 
 const addNewPlayer = () => {
@@ -23,6 +27,44 @@ const addNewPlayer = () => {
     if (playerBox.childElementCount > 5) {
         addPlayer.classList.add('hide')
     }
+}
+
+const editPlayerLogo = (e) => {
+    if (e.target.matches('.player__profile__logo')) {
+        tooltip = e.target.querySelector('.tooltip')
+        tooltip.classList.toggle('hide')
+        return
+    }
+    if (e.target.matches('.tooltip__color')) {
+        changeColor(e)
+    }
+}
+
+const changeColor = (e) => {
+    const profileLogo = e.target.closest('.player__profile__logo')
+    switch (true) {
+        case e.target.classList.contains('blue'):
+            profileLogo.classList.remove('blue', 'red', 'green', 'yellow', 'black')
+            profileLogo.classList.add('blue')
+            break
+        case e.target.classList.contains('red'):
+            profileLogo.classList.remove('blue', 'red', 'green', 'yellow', 'black')
+            profileLogo.classList.add('red')
+            break
+        case e.target.classList.contains('green'):
+            profileLogo.classList.remove('blue', 'red', 'green', 'yellow', 'black')
+            profileLogo.classList.add('green')
+            break
+        case e.target.classList.contains('yellow'):
+            profileLogo.classList.remove('blue', 'red', 'green', 'yellow', 'black')
+            profileLogo.classList.add('yellow')
+            break
+        case e.target.classList.contains('black'):
+            profileLogo.classList.remove('blue', 'red', 'green', 'yellow', 'black')
+            profileLogo.classList.add('black')
+            break
+    }
+    e.target.closest('.tooltip').classList.add('hide')
 }
 
 document.addEventListener('DOMContentLoaded', main)
